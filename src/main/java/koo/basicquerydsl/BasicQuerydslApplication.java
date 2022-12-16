174b2c74635807607a5aa553f2cfe79eab43765a
rngustj9139 <mnb9139@khu.ac.kr>
2022-12-17 00:30:18 +0900

    First setting

---
+++ src/main/java/koo/basicquerydsl/BasicQuerydslApplication.java
@@ -0,0 +1,13 @@
+package koo.basicquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicQuerydslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koo/basicquerydsl/BasicQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.basicquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
