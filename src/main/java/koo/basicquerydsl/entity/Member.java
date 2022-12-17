098a6a6542c6f885e22e6ce5c19b4603060785c8
rngustj9139 <mnb9139@khu.ac.kr>
2022-12-18 05:14:58 +0900

    Add tests for jpql and queryDSL

--- src/main/java/koo/basicquerydsl/entity/Member.java
+++ src/main/java/koo/basicquerydsl/entity/Member.java
@@ -22,6 +22,14 @@ public class Member {
     @JoinColumn(name = "team_id")
     private Team team;
 
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
     public Member(String username, int age, Team team) {
         this.username = username;
         this.age = age;
@@ -30,7 +38,7 @@ public class Member {
         }
     }
 
-    // 양방향 연관관계 메서드
+    // 연관관계 편의 메서드
     public void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
---
+++ src/test/java/koo/basicquerydsl/QuerydslBasicTest.java
@@ -0,0 +1,67 @@
+package koo.basicquerydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import koo.basicquerydsl.entity.Member;
+import koo.basicquerydsl.entity.QMember;
+import koo.basicquerydsl.entity.Team;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        // member1을 찾아라
+        Member findByMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        Assertions.assertThat(findByMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        // Q 타입을 통해 컴파일 시점에 에러를 잡을 수 있게 한다.
+        QMember m = new QMember("m");// QMember가 없을 경우 Tasks/Other/compileQuerydsl을 더블 클릭해야한다. (m은 어떤 QMember인지 이름을 부여한 것이다.)
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1")) // 파라미터 바인딩 처리를 통해 sql 인젝션을 예방
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+}
