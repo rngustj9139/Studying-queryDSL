91542562544f001a542e59f8f0b0e271f1077e2c
rngustj9139 <mnb9139@khu.ac.kr>
2022-12-19 04:28:24 +0900

    sort

--- src/test/java/koo/basicquerydsl/QuerydslBasicTest.java
+++ src/test/java/koo/basicquerydsl/QuerydslBasicTest.java
@@ -123,4 +123,36 @@ public class QuerydslBasicTest {
                 .fetchCount(); // select count(m) from Member m 수행
     }
 
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 오름차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     **/
+    @Test
+    public void sort() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QMember member = QMember.member;
+
+        // 테스트 데이터 더 추가하기
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(),
+                         member.username.asc().nullsLast()) // username이 null값이면 맨 마지막으로 오게 한다.
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        Assertions.assertThat(member5.getUsername()).isEqualTo("member5");
+        Assertions.assertThat(member6.getUsername()).isEqualTo("member6");
+        Assertions.assertThat(memberNull.getUsername()).isNull();
+    }
+
 }
